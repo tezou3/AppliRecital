76d1d106fd260709513be4d2b43d3877788789ad
matsumoto_y <matsumoto_y@skygroup.jp>
2018-02-04 03:19:56 +0900

    最初のひな型。用語登録のアクティビティまで飛べる。

---
+++ NotebookApplication/app/src/main/java/com/example/tezou/notebookapplication/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.tezou.notebookapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button registerButton = (Button)findViewById(R.id.to_register_button);
+        Button wordlistButton = (Button)findViewById(R.id.to_wordlist_button);
+        Button testButton = (Button)findViewById(R.id.to_test_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+//                処理
+                Intent intent = new Intent(MainActivity.this, RegisterWord.class);
+                // Activityの移動
+                startActivity(intent);
+            }
+        });
+
+        wordlistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+//                処理
+                Intent intent = new Intent(MainActivity.this, RegisterWord.class);
+                // Activityの移動
+                startActivity(intent);
+            }
+        });
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+//                処理
+                Intent intent = new Intent(MainActivity.this, RegisterWord.class);
+                // Activityの移動
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "onBackPressed", Toast.LENGTH_SHORT).show();
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Toast.makeText(this, event.toString(), Toast.LENGTH_SHORT).show();
+        return super.onTouchEvent(event);
+    }
+
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//    ここに保存する処理を入れる
+//        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+//        super.onDestroy();
+//    }
+//
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+//    }
+
+}
---
+++ NotebookApplication/app/src/main/java/com/example/tezou/notebookapplication/RegisterWord.java
@@ -0,0 +1,24 @@
+package com.example.tezou.notebookapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tezou on 2018/02/03.
+ */
+
+public class RegisterWord extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_word);
+    }
+
+    public String word;
+    public String description;
+
+    public void regist(){}
+
+
+}
